6186bd462e0080f1c301ab3b0afc0377a3eceaf9
labdatabit <ofjuancarlos1@gmail.com>
2023-08-15 00:06:33 -0400

    primer commit en rama1

--- pp.py
+++ pp.py
@@ -7,17 +7,25 @@ print("holax2")
 # un comentario
 #
 #
+
+
+5
+sd=85
+
 print(878)
 #otro comentario
 """ un comentario """
-'''un comentario'''
+'''un comentardio'''
 ordename=[1,2,3,1,2,3,2.3]
 print(ordename)
 print("hola jc")
 print(85*963)
 (ordename.sort())
+print(52)
 
 print(89*8)
+
+hola=5
 
 print(ordename)
 '''soy un comentario de bloque
