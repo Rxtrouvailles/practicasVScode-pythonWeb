aa93cc4bb3211fc336f69fa24034db65113d1c35
labdatabit <ofjuancarlos1@gmail.com>
2023-08-16 22:49:12 -0400

    asda

--- pp.py
+++ pp.py
@@ -8,14 +8,15 @@
 print(84)
 
 sd=85
-sdas4=15
 
+sdas4=15
+juan case
 print(878)
 #otro comentario
 """ un comentario """
 '''un comentardio'''
 ordename=[1,2,3,1,2,3,2.3]
-listica=84
+listica={4,6,5}
 print(ordename)
 print("hola jc")
 print(85*963)
@@ -26,7 +27,6 @@ print(89*8)
 
 hola=585
 
-print(ordename)
 '''soy un comentario de bloque
 ****************************
 '''
@@ -34,3 +34,4 @@ print("aqui termino mi super algoritmo")
 
 
 
+d 
